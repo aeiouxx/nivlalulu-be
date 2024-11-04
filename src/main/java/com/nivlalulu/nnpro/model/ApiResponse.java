e86d2649f8b5c7ea96c400c6b876638d830d6aa1
st61057 <st61057@upce.cz>
2024-11-05 00:19:00 +0100

    Api response

---
+++ src/main/java/com/nivlalulu/nnpro/model/ApiResponse.java
@@ -0,0 +1,15 @@
+package com.nivlalulu.nnpro.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int status;
+    private String message;
+    private Object result;
+}
