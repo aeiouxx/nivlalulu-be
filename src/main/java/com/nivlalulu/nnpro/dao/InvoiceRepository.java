8137286289cff914c42adf0b98e55b36e5b46794
st61057 <st61057@upce.cz>
2024-11-05 00:57:26 +0100

    Repo

--- src/main/java/com/nivlalulu/nnpro/dao/InvoiceRepository.java
+++ src/main/java/com/nivlalulu/nnpro/dao/InvoiceRepository.java
@@ -1,13 +1,15 @@
 package com.nivlalulu.nnpro.dao;
 
 import com.nivlalulu.nnpro.model.Invoice;
-import com.nivlalulu.nnpro.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+
+    Optional<Invoice> findById(UUID id);
 
     List<Invoice> findAllByCompanyName(String companyName);
 
