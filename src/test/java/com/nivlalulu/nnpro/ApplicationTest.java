55612833309c13b0267cbd75e5608612d4de0da0
aeiouxx <st55776@upce.cz>
2024-11-26 02:18:58 +0100

    build: add testcontainers, spring profiles, dockerfile

--- src/test/java/com/nivlalulu/nnpro/ApplicationTest.java
+++ src/test/java/com/nivlalulu/nnpro/ApplicationTest.java
@@ -10,13 +10,11 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 
-@SpringBootTest(classes = Application.class)
+@SpringBootTest
 @ActiveProfiles("test")
 public class ApplicationTest {
     @Test
     public void contextLoads() {
-        System.out.println("Test context loaded successfully, good job me!");
-        Assert.isTrue(false, "This is a test failure message, " +
-                "testing report generation on failed tests.");
+
     }
 }
