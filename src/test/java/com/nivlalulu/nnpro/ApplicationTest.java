b9973db22ebb2bbb95b99fd3598152e36db1210d
st61057 <st61057@upce.cz>
2024-11-26 15:13:40 +0100

    test

--- src/test/java/com/nivlalulu/nnpro/ApplicationTest.java
+++ src/test/java/com/nivlalulu/nnpro/ApplicationTest.java
@@ -1,5 +1,8 @@
 package com.nivlalulu.nnpro;
 
+import com.nivlalulu.nnpro.dto.ProductDto;
+import com.nivlalulu.nnpro.service.InvoiceService;
+import com.nivlalulu.nnpro.service.ProductService;
 import io.jsonwebtoken.lang.Assert;
 import org.hibernate.cfg.Environment;
 import org.junit.jupiter.api.Test;
@@ -8,15 +11,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("test")
 public class ApplicationTest {
+
+    @Autowired
+    private InvoiceService invoiceService;
+
+    @Autowired
+    private ProductService productService;
+
     @Test
     public void contextLoads() {
         System.out.println("Test context loaded successfully, good job me!");
         Assert.isTrue(false, "This is a test failure message, " +
                 "testing report generation on failed tests.");
+    }
+
+    @Test
+    public void testCorrectSaveProduct() {
+
+        ProductDto newProduct = new ProductDto();
+        newProduct.setName("testProduct");
+        newProduct.setQuantity(2);
+        newProduct.setPrice(BigDecimal.valueOf(100));
+        newProduct.setTax(BigDecimal.valueOf(21));
+        newProduct.setTotal(BigDecimal.valueOf(121));
+
+        ProductDto productCreated = productService.createProduct(newProduct);
+
+        Assert.isTrue(productCreated != null, "Product succesfully created");
     }
 }
