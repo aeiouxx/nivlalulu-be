237420e7586dc70dec8a982972bf8ae1c4c74d6e
st61057 <st61057@upce.cz>
2024-12-12 12:21:39 +0100

    fix mailu

--- src/main/java/com/nivlalulu/nnpro/service/MailService.java
+++ src/main/java/com/nivlalulu/nnpro/service/MailService.java
@@ -1,27 +1,29 @@
 package com.nivlalulu.nnpro.service;
 
 
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.simplejavamail.email.EmailBuilder;
 
 public class MailService {
 
-
     private void sendEmail(String userEmail, String resetCode) {
-//        Email email = EmailBuilder.startingBlank()
-//                .from("")
-//                .to(userEmail)
-//                .withSubject("Reset code")
-//                .withPlainText("Reset code: " + resetCode)
-//                .buildEmail();
-//
-//
-//        Mailer mailer = MailerBuilder
-//                .withSMTPServer("smtp.gmail.com", 587, "", "")
-//                .withTransportStrategy(TransportStrategy.SMTP_TLS)
-//                .withDebugLogging(true)
-//                .buildMailer();
-//
-//        mailer.sendMail(email);
+        Email email = EmailBuilder.startingBlank()
+                .from("")
+                .to(userEmail)
+                .withSubject("Reset code")
+                .withPlainText("Reset code: " + resetCode)
+                .buildEmail();
+
+
+        Mailer mailer = MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 587, "", "")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .withDebugLogging(true)
+                .buildMailer();
+
+        mailer.sendMail(email);
     }
-
-
 }
