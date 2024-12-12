266e5b2633a3364a80d9278bafcb77a7894440b5
st61057 <st61057@upce.cz>
2024-12-12 20:31:09 +0100

    mail service

---
+++ src/main/java/com/nivlalulu/nnpro/service/MailService.java
@@ -0,0 +1,27 @@
+package com.nivlalulu.nnpro.service;
+
+
+
+public class MailService {
+
+
+    private void sendEmail(String userEmail, String resetCode) {
+//        Email email = EmailBuilder.startingBlank()
+//                .from("")
+//                .to(userEmail)
+//                .withSubject("Reset code")
+//                .withPlainText("Reset code: " + resetCode)
+//                .buildEmail();
+//
+//
+//        Mailer mailer = MailerBuilder
+//                .withSMTPServer("smtp.gmail.com", 587, "", "")
+//                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+//                .withDebugLogging(true)
+//                .buildMailer();
+//
+//        mailer.sendMail(email);
+    }
+
+
+}
