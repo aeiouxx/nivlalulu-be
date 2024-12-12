2aa465de91be7873a87257906dfcd8af134f4760
st61057 <st61057@upce.cz>
2024-12-12 16:06:51 +0100

    změna userDto

--- src/main/java/com/nivlalulu/nnpro/dto/UserDto.java
+++ src/main/java/com/nivlalulu/nnpro/dto/UserDto.java
@@ -1,13 +1,11 @@
 package com.nivlalulu.nnpro.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigInteger;
-import java.util.UUID;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,23 +13,9 @@ import java.util.UUID;
 public class UserDto {
 
     @JsonIgnore
-    private UUID id;
+    private Long id;
 
-    private String type;
-
-    @JsonProperty("organization_name")
-    private String organizationName;
-
-    @JsonProperty("contact_person")
-    private String contactPerson;
-
-    private String address;
-
-    private String country;
-
-    private String companyId;
-
-    private String taxId;
+    private String username;
 
     private BigInteger phone;
 
