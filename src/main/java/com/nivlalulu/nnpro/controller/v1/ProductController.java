7652a1dac9270710d7204c2cf592465d5ac85c02
aeiouxx <st55776@upce.cz>
2024-12-14 22:19:28 +0100

    refactor: move controllers to v1, separate exposed api and authed api, change @Autowired to constructor injection

--- src/main/java/com/nivlalulu/nnpro/controller/v1/ProductController.java
+++ src/main/java/com/nivlalulu/nnpro/controller/v1/ProductController.java
@@ -1,9 +1,10 @@
-package com.nivlalulu.nnpro.controller;
+package com.nivlalulu.nnpro.controller.v1;
 
 import com.nivlalulu.nnpro.dto.v1.ProductDto;
 import com.nivlalulu.nnpro.dto.ApiResponse;
 import com.nivlalulu.nnpro.service.impl.ProductService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
@@ -14,11 +15,10 @@ import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/v1/product")
+@RequiredArgsConstructor
 @Validated
 public class ProductController {
-
-    @Autowired
     private ProductService productService;
 
     @PostMapping("/saveProduct")
