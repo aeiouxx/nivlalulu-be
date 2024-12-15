cd97ea1f25eca2561d1effe0a67aebcc57a1bb3a
aeiouxx <st55776@upce.cz>
2024-12-15 20:42:07 +0100

    delete unused interface

--- src/main/java/com/nivlalulu/nnpro/service/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.nivlalulu.nnpro.service;
-
-import com.nivlalulu.nnpro.dto.v1.InvoiceItemDto;
-
-// todo: add methods to this interface
-public interface IProductService {
-    InvoiceItemDto createProduct(InvoiceItemDto productDto);
-}
