c48ecfc3a2350e88e284394ed7c0a529f3c44f04
aeiouxx <st55776@upce.cz>
2024-12-21 12:06:44 +0100

    fix: make swagger visibility depend on correct property

--- src/main/java/com/nivlalulu/nnpro/configuration/SwaggerConfiguration.java
+++ src/main/java/com/nivlalulu/nnpro/configuration/SwaggerConfiguration.java
@@ -13,8 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@ConditionalOnExpression("${swagger.enabled:true}")
-@Profile("dev")
+@ConditionalOnExpression("${springdoc.api-docs.enabled:true}")
 public class SwaggerConfiguration {
     private static final String SECURITY_SCHEME_KEY = "bearerAuth";
 
